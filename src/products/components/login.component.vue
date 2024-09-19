7e15ececedbf462ebea934758634bd816eac7200
AnthonyHuapaya <u202220235@upc.edu.pe>
2024-09-19 12:59:01 -0500

    actualizarRama

--- src/products/components/login.component.vue
+++ src/products/components/login.component.vue
@@ -1,15 +1,60 @@
 <script>
+//import logoImage from 'src/assets/logo.png';
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      //logo: logoImage,
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="loginUsername">
-    Usuario: 
+  <div class="containerLogin">
+    <div class="logo">
+      <img :src="logo" alt="Logo" />
+    </div>
+    <div class="loginUsername">
+      <h1>Username:</h1>
+      <input class="textboxUsername" type="text" v-model="username" id="username" placeholder="Enter your username" maxlength="20" />
+    </div>
+    <div class="loginPassword">
+      <h1>Password:</h1>
+      <input class="textboxUsername" type="password" v-model="password" id="password" placeholder="Enter your password" maxlength="20" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.containerLogin {
+  margin: auto;
+}
+  .loginUsername {
+    margin-left: 405px;
+    text-align: left;
+    font-weight: bolder;
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 7px;
+  }
+  .loginPassword {
+    margin-left: 405px;
+    text-align: left;
+    font-weight: bolder;
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 7px;
+  }
+  .textboxUsername {
+    width: 50%;
+    height: 50px;
+    font-size: 13px;
+    border-radius: 15px;
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    margin: auto;
+  }
+  .logo {
 
+  }
 </style>

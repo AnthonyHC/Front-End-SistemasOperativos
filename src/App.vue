2f8c1a46b5f13ede64ec33d13435d05b2f6e904c
AnthonyHuapaya <u202220235@upc.edu.pe>
2024-09-20 10:54:54 -0500

    update components

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import Toolbar from "./products/components/toolbar.component.vue";
 import Login from './products/components/login.component.vue';
 import ViProductCard from "./products/components/view-product-card.component.vue";
+import ShopCar from "./products/components/shop-car.component.vue";
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import ViProductCard from "./products/components/view-product-card.component.vue
   <h2>Soy Eli</h2>
   <login></login>
   <vi-product-card></vi-product-card>
+  <shop-car></shop-car>
 </template>
 
 <style scoped>
--- src/products/components/shop-car.component.vue
+++ src/products/components/shop-car.component.vue
@@ -1,13 +1,74 @@
 <script>
+
 export default {
-  name: "shop-car.component"
+  name: "ShopCar",
+  data() {
+    return {
+      items: [
+        { product: 'Juan', cant: 30, price: 11.50 },
+        { product: 'Ana', cant: 25, price: 5.50 },
+        { product: 'Carlos', cant: 40, price: 8.70 },
+      ],
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.items.reduce((sum, item) => sum + item.price, 0).toFixed(2);
+    }
+  }
 }
 </script>
 
 <template>
-
+  <div class="container">
+    <pv-table :value="items" class="elevation-1">
+      <pv-column field="product" header="Producto">
+        <template #body="{ data }">
+          <div class="product-cell">
+            <img src="../../assets/logo.png" alt="Imagen" class="product-image" />
+            {{ data.product }}
+          </div>
+        </template>
+      </pv-column>
+      <pv-column field="cant" header="Cantidad"></pv-column>
+      <pv-column field="price" header="Precio"></pv-column>
+    </pv-table>
+    <div class="total-row">
+      <span class="total-label">Total:</span>
+      <span class="total-value">{{ totalPrice }}</span>
+    </div>
+  </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.container {
+  margin: 0 auto;
+  padding: 16px;
+  max-width: 1200px;
+}
+.elevation-1 {
+  margin-top: 20px;
+}
+.product-cell {
+  display: flex;
+  align-items: center;
+}
+.product-image {
+  width: 60px; /* Ajusta el tamaño de la imagen */
+  height: auto; /* Ajusta el tamaño de la imagen */
+  margin-right: 10px; /* Espacio entre la imagen y el texto */
+}
+.total-row {
+  margin-top: 20px;
+  display: flex;
+  justify-content: flex-end;
+}
+.total-label {
+  font-weight: bolder;
+  margin-right: 10px;
+}
+.total-value {
+  font-weight: bolder;
+}
 </style>
